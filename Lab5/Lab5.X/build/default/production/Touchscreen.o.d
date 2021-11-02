3f3bc0357b222af5c4fca1d45f1f1d8a41fef2a2
Wand2843 <70994859+Wand2843@users.noreply.github.com>
2021-11-02 15:26:41 -0400

    Delete Lab5 directory

--- Lab5/Lab5.X/build/default/production/Touchscreen.o.d
+++
@@ -1,6 +0,0 @@
-build/default/production/Touchscreen.o: Touchscreen.c TouchScreen.h \
- adc_intf.h
-
-TouchScreen.h:
-
-adc_intf.h:
--- Lab5/Lab5.X/build/default/production/tft_gfx.o.d
+++
@@ -1,8 +0,0 @@
-build/default/production/tft_gfx.o: tft_gfx.c tft_gfx.h glcdfont.h \
- tft_master.h
-
-tft_gfx.h:
-
-glcdfont.h:
-
-tft_master.h:
